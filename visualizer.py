0fb3796b0fc2818e02bf86649f4097fa809bc8bc
belliion <bellion2609@gmail.com>
2025-06-01 14:57:45 +0700

    Added momentum and KE

--- visualizer.py
+++ visualizer.py
@@ -13,23 +13,27 @@ class Visualizer():
         
         x,y = ball.pos
         vx,vy = ball.vel
-        mass = None # to be added
+        mass = ball.mass # to be added
 
         speed = math.hypot(vx,vy)
+        momentum = speed * mass
+        kinetic_energy = 0.5 * mass * speed ** 2
 
         if speed > 0.01:
             angle_deg = (math.degrees(math.atan2(vy, vx)) + 360) % 360
-            direction_str = f"dir={angle_deg:.1f}°"
+            direction_str = f"dir: {angle_deg:.1f}°"
         else:
-            direction_str = "dir=0.0°"
+            direction_str = "dir: 0.0°"
 
         text_infos = [
             f"speed: {speed:.2f}",
+            f"mmtm: {momentum:.2f}",
+            f"KE: {kinetic_energy:.2f}",
             direction_str
         ]
 
         for i, text in enumerate(text_infos):
             label = font.render(text, True, font_color)
-            screen.blit(label, (x + 10, y + i * 15))
+            screen.blit(label, (x + 15, y + i * 15 - 20))
 
         return 
